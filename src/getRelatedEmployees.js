6ed991be756a7e563d0c9ea5083a295792f4769c
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-29 16:20:35 -0300

    Cria função isManager

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const managers = [
+    '9e7d4524-363c-416a-8759-8aa7e50c0992',
+    'fdb2543b-5662-46a7-badc-93d960fdc0a8',
+    '0e7b460e-acf4-4e17-bcb3-ee472265db83',
+  ];
+
+  return managers.includes(id);
 }
 
 function getRelatedEmployees(managerId) {
