3d4b807f86b5b764321859dd914799f841b59f92
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-29 17:12:12 -0300

    Cria função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const animalCount = {};
+    data.species.forEach((specie) => {
+      animalCount[specie.name] = specie.residents.length;
+    });
+    return animalCount;
+  }
+  if (Object.prototype.hasOwnProperty.call(animal, 'specie')) {
+    const { residents } = data.species.find((specie) => specie.name === animal.specie);
+    if (Object.prototype.hasOwnProperty.call(animal, 'sex')) {
+      const residentSexFilter = (resident) => resident.sex === animal.sex;
+      return residents.filter(residentSexFilter).length;
+    }
+    return residents.length;
+  }
 }
 
 module.exports = countAnimals;
