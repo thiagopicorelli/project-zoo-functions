46d1d39380103bc95e13985e3ba9b912e56ec190
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-29 15:43:32 -0300

    Implementa função getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const specieObj = data.species.find((specie) => specie.name === animal);
+  const youngerAnimal = specieObj.residents.find((resident) => resident.age < age);
+
+  if (youngerAnimal === undefined) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = getAnimalsOlderThan;
