8396204a17d1883d6d9b9a509569206e7e2e9e42
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-29 17:14:43 -0300

    Usa every em getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,12 +2,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const specieObj = data.species.find((specie) => specie.name === animal);
-  const youngerAnimal = specieObj.residents.find((resident) => resident.age < age);
-
-  if (youngerAnimal === undefined) {
-    return true;
-  }
-  return false;
+  return specieObj.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
