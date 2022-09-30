4e6894c045a0500a06126c907fcdae946224f376
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-30 13:38:08 -0300

    Adiciona função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const specieId = data.employees.find((employ) => employ.id === id).responsibleFor[0];
+  const specieObj = data.species.find((specie) => specie.id === specieId);
+  const oldest = specieObj.residents.sort((a, b) => b.age - a.age)[0];
+
+  return [oldest.name, oldest.sex, oldest.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
