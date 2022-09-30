ac899f6c5de4f188e0d0d3d8d75b69a3894da237
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-30 15:36:58 -0300

    Cria função getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
+function getResidents(residents, sex) {
+  if (sex !== undefined) {
+    return residents.filter((res) => res.sex === sex);
+  }
+  return residents;
+}
+
+function getResidentNames(specie, residents, includeNames, sorted) {
+  if (includeNames) {
+    let mapList = residents.map((res) => res.name);
+    if (sorted) {
+      mapList = mapList.sort();
+    }
+    return { [specie.name]: mapList };
+  }
+  return specie.name;
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  const mapObj = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+  const { includeNames, sorted, sex } = options !== undefined ? options : [false, false, undefined];
+  data.species.forEach((specie) => {
+    const residents = getResidents(specie.residents, sex);
+    mapObj[specie.location].push(getResidentNames(specie, residents, includeNames, sorted));
+  });
+  return mapObj;
 }
 
 module.exports = getAnimalMap;
